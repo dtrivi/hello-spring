cd3e0bdfc3815c3aead135fad81857e403a0e658
dtrivi <danieltrivinos@gmail.com>
2021-03-07 21:59:53 -0600

    10.4.1. Controller Classes

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,25 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller //To designate a given class as a controller within the Spring framework, place @Controller annotation atop controller class
+@ResponseBody //Since each method within the class uses @ResponseBody, we can put it above the class instead of above each class. This annotation tells Spring Boot that this method will return a plain text HTTP response. Doesn't use HTML.
+@RequestMapping("hello") //Says every single method within class should begin with /hello . So any info we put in after it, like /goodbye, now lives at /hello/goodbye
 public class HelloController {
 
 //    //Handle requests at the root path (doesn't have routing info in method) :
 //    @GetMapping //annotation tells Spring Boot that this method will only accept 'get requests'.
-//    @ResponseBody //annotation tells Spring Boot that this method will return a plain text HTTP response. Doesn't use HTML.
 //    public String hello() {
 //        return "Hello, Spring!";
 //    }
 
 //    //Handle requests at path /hello :
 //    @GetMapping("hello")
-//    @ResponseBody
 //    public String hello() {
 //        return "Hello, Spring!";
 //    }
 
     //Method says 'goodbye' instead of 'hello' :
+    //lives at /hello/goodbye
     @GetMapping("goodbye")
-    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
@@ -30,15 +30,14 @@ public class HelloController {
     //Handle requests from query parameters of the form /hello?name=LaunchCode :
     // (note that /hello has to be commented out to run this method since they live at the same path)
 //    @GetMapping("hello")
+    //lives at /hello/hello
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello") //@RequestMapping is more general mapping annotation. Putting the types or requests in annotation params specifies which types of requests a @RequestMapping will accept
-    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) { //@RequestParam signifies to Spring that this method expects a query param called 'name'. query param and method param have to match
         return "Hello, " + name + "!";
     }
 
     //Handle requests where variable is part path, not query ... of the form /hello/LaunchCode :
-    @GetMapping("hello/{name}") //put '/' after hello and variable 'name' in curly brackets to signify you want this to accept piece of data in path
-    @ResponseBody
+    @GetMapping("{name}") //put '/' after hello and variable 'name' in curly brackets to signify you want this to accept piece of data in path
     public String helloWithPathParam(@PathVariable String name) { //@PathVariable tells SpringBoot that handler method is looking for a request to /hello/name where the name can be anything and we're going to take the value of that 'name' and place it in the method param 'name'.
         return "Hello, " + name + "!";
     }
@@ -46,7 +45,6 @@ public class HelloController {
 //    //Method to display a form :
 //    //*note: form element itself (line 55) does not have a method attribute like 'method='get''. default method is GET, which we're set up for with our helloWithQueryParam handler being set up with @GetMapping for get requests
 //    @GetMapping("form")
-//    @ResponseBody
 //    public String helloForm() {
 //        return "<html>" +
 //                "<body>" +
@@ -58,8 +56,8 @@ public class HelloController {
 //                "</html>";
 //    }
 
+    //lives at /hello/form
     @GetMapping("form")
-    @ResponseBody
     public String helloForm() {
         return "<html>" +
                 "<body>" +
